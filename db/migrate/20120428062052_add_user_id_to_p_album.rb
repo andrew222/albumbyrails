3adbcff0eaf4e7a8b4b57d301d0a8d2854c872bc
andrew222 <zzcv20051122@126.com>
2012-12-10 21:47:46 +0800

    delete fold migrate

--- db/migrate/20120428062052_add_user_id_to_p_album.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToPAlbum < ActiveRecord::Migration
-  def change
-    add_column :p_albums, :user_id, :integer
-
-  end
-end
--- db/migrate/20120504053620_add_p_album_id_to_upload_photo.rb
+++
@@ -1,6 +0,0 @@
-class AddPAlbumIdToUploadPhoto < ActiveRecord::Migration
-  def change
-    add_column :upload_photos, :p_album_id, :integer
-
-  end
-end
